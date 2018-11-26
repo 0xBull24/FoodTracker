728373a34364471189a5c77bf64346a1d452f1a8
DBiles <Darron.biles@gmail.com>
2018-11-26 06:48:15 +0000

    Long night with the PB. Finally got it running

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'burger_db'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log(`Connected to the database on id ${connection.threadId}`);
+});
